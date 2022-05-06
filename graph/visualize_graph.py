674f094c7111624a3f9ff5bf15b89a655d8fbedf
youngdashu <anowak2000@gmail.com>
2022-05-06 23:59:58 +0200

    node2vec changes

--- graph/visualize_graph.py
+++ graph/visualize_graph.py
@@ -2,8 +2,41 @@ import tempfile
 
 from graph.ProvinceGraph import ProvinceGraph
 
+import matplotlib.pyplot as plt
+
 
 def visualize_graph(graph: ProvinceGraph):
     graphviz_graph = graph.graphviz_graph()
 
     graphviz_graph.view(tempfile.mktemp('.gv'))
+
+def visualize_graph_vectorized(file_name='./graph/out2.emd'):
+
+    with open(file_name, 'r') as f:
+        node_count, dims = tuple(f.readline().split(' '))
+        node_count = int(node_count)
+
+        nodes = [None] * node_count
+
+
+        for line in f.readlines():
+
+            node_id, x, y = tuple(line.split(' '))
+            node_id = int(node_id)
+            x = float(x)
+            y = float(y)
+
+            nodes[node_id] = x, y
+
+        xs, ys = tuple(zip(*nodes))
+        plt.scatter(list(xs), list(ys))
+
+        for node_id, coords in enumerate(nodes):
+            plt.annotate(str(node_id), coords)
+
+        plt.show()
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from graph.ProvinceGraph import ProvinceGraph
 from graph.read_graph import read_graph
-from graph.visualize_graph import visualize_graph
+from graph.visualize_graph import visualize_graph, visualize_graph_vectorized
 
 if __name__ == "__main__":
+
+
+    # visualize_graph_vectorized()
+
+    # exit(0)
 
     graph: ProvinceGraph = read_graph('./graph/input.txt')
 
     visualize_graph(graph)
+
+
